b2b7e113cc17a079b25a9188b73bfd162481f039
Wolfgang Grieskamp <wgrieskamp@gmail.com>
2022-04-20 01:17:25 +0800

    [repo] Remove workspace hack. (#55)
    * [repo] Remove workspace hack.
    
    Workspace hack is a [build optimizer](https://docs.rs/cargo-hakari/latest/cargo_hakari/about/index.html) which avoids building different feature combinations for different build commands in one workspace. As the link shows, it has advantages for certain build scenarios.
    
    However, it also has disadvantages when building crosscuts through our repo. For example, even if one doesn't ever need the EVM feature of move-cli, workspace-hack will trigger to build it anyway.
    
    This PR removes workspace-hack. This should lead to faster build time if one does repeated builds of one particular crate (the usual dev scenario), and *may* result in slower builds if one often switches the cargo build target (more unusual dev scenario). For building the full workspace, no difference in timing is expected, as with one build command, cargo always builds the sum of all features. This has been verified on a Ryzen 5950x (16 core):
    
    ```
    With WH:
    real    1m16.463s
    user    11m14.493s
    sys     1m8.965s
    
    Without WH:
    ```
    real    1m12.165s
    user    11m8.295s
    sys     1m10.649s
    ```
    
    * [repo] Remove hakari dependency from devtools/x. Keeping this code would require us to also have an `[hakari]` config in x.toml, which need to point to a valid workspace-hack package, which does not longer exist.
    
    * [repo] Now that workspace hack is gone, we can use cargo udeps to purge unused deps. It weren't that many, actually.
    
    fixup
    
    fix compile error

--- language/testing-infra/transactional-test-runner/src/framework.rs
+++ language/testing-infra/transactional-test-runner/src/framework.rs
@@ -9,6 +9,7 @@ use crate::tasks::{
     RunCommand, SyntaxChoice, TaskCommand, TaskInput, ViewCommand,
 };
 use anyhow::{anyhow, Result};
+use clap::Parser;
 use move_binary_format::{
     binary_views::BinaryIndexedView,
     file_format::{CompiledModule, CompiledScript},
