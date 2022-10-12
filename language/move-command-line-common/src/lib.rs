ab0d68b147f68dde0600bfd987e5788d377da1d9
LemonHX <lemonhx@lemonhx.tech>
2022-10-12 20:31:42 +0800

    move command line common no std

--- language/move-command-line-common/src/lib.rs
+++ language/move-command-line-common/src/lib.rs
@@ -3,12 +3,17 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #![forbid(unsafe_code)]
+#![cfg_attr(any(feature = "nostd"), no_std)]
+#[cfg(feature = "nostd")]
+extern crate alloc;
 
 pub mod address;
 pub mod character_sets;
+#[cfg(not(feature = "nostd"))]
 pub mod env;
 pub mod files;
 pub mod parser;
+#[cfg(not(feature = "nostd"))]
 pub mod testing;
 pub mod types;
 pub mod values;
