fa2021ce187aa1c4b1ed0a3956dbbfa59c3fd484
nk_ysg <nk_ysg@163.com>
2022-10-25 20:21:17 +0800

    add native functions

--- language/move-stdlib/src/natives/hash.rs
+++ language/move-stdlib/src/natives/hash.rs
@@ -61,33 +61,6 @@ pub fn make_native_sha2_256(gas_params: Sha2_256GasParameters) -> NativeFunction
     )
 }
 
-// pub fn native_keccak_256(
-//     context: &mut NativeContext,
-//     _ty_args: Vec<Type>,
-//     mut arguments: VecDeque<Value>,
-// ) -> PartialVMResult<NativeResult> {
-//     debug_assert!(_ty_args.is_empty());
-//     debug_assert!(arguments.len() == 1);
-//
-//     let hash_arg = pop_arg!(arguments, Vec<u8>);
-//
-//     let cost = native_gas(
-//         context.cost_table(),
-//         NativeCostIndex::KECCAK_256,
-//         hash_arg.len(),
-//     );
-//     let output = {
-//         let mut output = [0u8; 32];
-//         let mut keccak = tiny_keccak::Keccak::v256();
-//         keccak.update(hash_arg.as_slice());
-//         keccak.finalize(&mut output);
-//         output.to_vec()
-//     };
-//
-//     Ok(NativeResult::ok(cost, smallvec![Value::vector_u8(output)]))
-// }
-//
-
 /***************************************************************************************************
  * native fun sha3_256
  *
