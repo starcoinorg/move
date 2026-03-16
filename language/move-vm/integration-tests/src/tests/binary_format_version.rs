f1478f34e93f85d06bfd611a2a6aba0bcb4838c0
simonjiao <simonjiaoh@gmail.com>
2026-03-17 04:13:44 +0800

    vm: route module publishing through staging storage

--- language/move-vm/integration-tests/src/tests/binary_format_version.rs
+++ language/move-vm/integration-tests/src/tests/binary_format_version.rs
@@ -145,6 +145,84 @@ fn test_publish_module_with_custom_max_binary_format_version() {
 }
 
 #[test]
+fn test_publish_module_with_custom_max_binary_format_version_lazy_loading() {
+    let m = basic_test_module();
+    let mut b_new = vec![];
+    let mut b_old = vec![];
+    m.serialize_for_version(Some(VERSION_MAX), &mut b_new)
+        .unwrap();
+    m.serialize_for_version(Some(VERSION_MAX.checked_sub(1).unwrap()), &mut b_old)
+        .unwrap();
+
+    {
+        let storage = InMemoryStorage::new();
+        let vm = MoveVM::new_with_config(
+            move_stdlib::natives::all_natives(
+                AccountAddress::from_hex_literal("0x1").unwrap(),
+                move_stdlib::natives::GasParameters::zeros(),
+            ),
+            VMConfig {
+                enable_lazy_loading: true,
+                ..Default::default()
+            },
+        )
+        .unwrap();
+        let mut sess = vm.new_session(&storage);
+
+        sess.publish_module(
+            b_new.clone(),
+            *m.self_id().address(),
+            &mut UnmeteredGasMeter,
+        )
+        .unwrap();
+        sess.publish_module(
+            b_old.clone(),
+            *m.self_id().address(),
+            &mut UnmeteredGasMeter,
+        )
+        .unwrap();
+    }
+
+    {
+        let storage = InMemoryStorage::new();
+        let vm = MoveVM::new_with_config(
+            move_stdlib::natives::all_natives(
+                AccountAddress::from_hex_literal("0x1").unwrap(),
+                move_stdlib::natives::GasParameters::zeros(),
+            ),
+            VMConfig {
+                deserializer_config: DeserializerConfig::new(
+                    VERSION_MAX.checked_sub(1).unwrap(),
+                    IDENTIFIER_SIZE_MAX,
+                ),
+                enable_lazy_loading: true,
+                ..Default::default()
+            },
+        )
+        .unwrap();
+        let mut sess = vm.new_session(&storage);
+
+        assert_eq!(
+            sess.publish_module(
+                b_new.clone(),
+                *m.self_id().address(),
+                &mut UnmeteredGasMeter,
+            )
+            .unwrap_err()
+            .major_status(),
+            StatusCode::UNKNOWN_VERSION
+        );
+
+        sess.publish_module(
+            b_old,
+            *m.self_id().address(),
+            &mut UnmeteredGasMeter,
+        )
+        .unwrap();
+    }
+}
+
+#[test]
 fn test_run_script_with_custom_max_binary_format_version() {
     let s = basic_test_script();
     let mut b_new = vec![];
